b11f85bfc7f490fed80cfce3bd16b9601bb170da
Soumya Xavier <denofsoumya@gmail.com>
2021-02-04 17:16:30 +0530

    first commit4

---
+++ components/MyHeader.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {Header,Icon} from 'react-native-elements';
+import {Text,View,StyleSheet} from 'react-native';
+
+const MyHeader = props => {
+    return(
+        <Header
+        centerComponent={{text: props.title, style:{color:"#90A5A9", fontSize:20, fontWeight:"bold"}}}
+        backgroundColor= "#eaf8fe"/>
+    )
+}
+
+export default MyHeader;
